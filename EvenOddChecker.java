f3e2173c608da39c69262ed7208e4590d0518bd4
leenahinduja <leenahinduja04@gmail.com>
2024-10-10 13:11:15 +0530

    Create EvenOddChecker.java

---
+++ EvenOddChecker.java
@@ -0,0 +1,11 @@
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        int number = 10;  // Change this number to check other values
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
